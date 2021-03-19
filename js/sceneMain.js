fde86b2d04f6e566d88466ce909e66902c4fc4ba
john kim <johnhkim124@yahoo.com>
2021-03-19 15:48:35 -0400

    first commit

---
+++ js/sceneMain.js
@@ -0,0 +1,16 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload()
+    {
+    	//load our images or sounds
+    }
+    create() {
+        //define our objects
+        console.log("Ready!");
+    }
+    update() {
+        //constant running loop
+    }
+}
